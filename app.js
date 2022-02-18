70c12b1e4e93a8e82a5bc908fbe5ece860192101
Serdar <serdar@gmail.com>
2022-02-19 00:39:55 +0100

    files changed

--- app.js
+++ app.js
@@ -1,79 +1,98 @@
-let ac = document.querySelector(".content1");
-let minusPlus= document.querySelector(".content2");
-let percent = document.querySelector(".content3");
-let divide = document.querySelector(".content4");
-let seven = document.querySelector(".content5");
-let eight = document.querySelector(".content6");
-let nine = document.querySelector(".content7");
-let multiply = document.querySelector(".content8");
-let four = document.querySelector(".content9");
-let five = document.querySelector(".content10");
-let six = document.querySelector(".content11");
-let minus = document.querySelector(".content12");
-let one = document.querySelector(".content13");
-let two = document.querySelector(".content14");
-let three = document.querySelector(".content15");
-let plus = document.querySelector(".content16");
-let zero = document.querySelector(".content17");;
-let dot = document.querySelector(".content18");
-let equal = document.querySelector(".content19");
-let result1 = document.getElementById("result1")
-let result2 = document.getElementById("result2")
+//selectıng html elements
+let container = document.querySelector(".container");
+let result1 = document.getElementById("result1");
+let result2 = document.getElementById("result2");
+
+//variables created
 let total = "";
 let sayi1 = "";
-let sayi2 = "";
-let operator = ""
-zero.addEventListener("click", () => {
-    sayi1 += "0";
-    result1.value = sayi1;
-    console.log(sayi1);
-})
-one.addEventListener("click", () => {
-    sayi1 += "1";
-    result1.value = sayi1;
-    console.log(sayi1);
-})
-two.addEventListener("click", () => {
-    sayi1 += "2";
-    result1.value = sayi1;
-    console.log(sayi1);
-})
-three.addEventListener("click", () => {
-    sayi1 += "3";
-    result1.value = sayi1;
-    console.log(sayi1);
-})
-four.addEventListener("click", () => {
-    sayi1 += "4";
-    result1.value = sayi1;
-    console.log(sayi1);
-})
-five.addEventListener("click", () => {
-    sayi1 += "5";
-    result1.value = sayi1;
-    console.log(sayi1);
-})
-six.addEventListener("click", () => {
-    sayi1 += "6";
-    result1.value = sayi1;
-    console.log(sayi1);
-})
-seven.addEventListener("click", () => {
-    sayi1 += "7";
-    result1.value = sayi1;
-    console.log(sayi1);
-})
-eight.addEventListener("click", () => {
-    sayi1 += "8";
-    result1.value = sayi1;
-    console.log(sayi1);
-})
-nine.addEventListener("click", () => {
-    sayi1 += "9";
-    result1.value = sayi1;
-    console.log(sayi1);
-})
-plus.addEventListener("click", () => {
-    operator += "+"
-    console.log(result1.value);
+let islem = [];
+
+//takıng action from user
+container.addEventListener("click", (e) => {
+
+
+//the area is calculate when user click operator button  
+    if (e.target.classList.contains('islem')){
+        
+        if (sayi1 != "" && total != "" ) {
+            switch (islem[0]){
+                case '+' : total = +total+(+sayi1) ;break;
+                case "-" : total = +total-(+sayi1);break;
+                case "/" : total = +total/(+sayi1);break;
+                case "*" : total = +total*(+sayi1);break;
+                case "%" : total = (+total*+sayi1)/(100);break;
+                case "=" : {
+                    if (islem[0] == "=" && sayi1 == "") {
+                        result1.value = "";
+                        result2.value = total;
+                    }
+                    else {
+                    switch (islem[0]){
+                        case '+' : total = +sayi1+(+sayi2);break;
+                        case "-" : total = s2-s1 ;break;
+                        case "/" : total = +sayi1/(+sayi2);break;
+                        case "*" : total = +sayi1*(+sayi2);break;
+                        case "%" : total = (+total*+sayi1)/(100);break;
+                        default:break;
+                    }    
+                    
+                    }
+                }
+                default:break;
+            }
+            sayi1 = "";
+        }
+        //when only one number entered
+        else if(total == "" ){
+            total = sayi1;
+            sayi1 = "";
+            result2.value = "";
+            
+        }
+        //add  new operator in array and remove old operator
+        islem.push(e.target.value);
+            while (islem.length > 1 ){
+                islem.shift();
+            }
+            //thıs block for when only get one number and press "=" button then  move on number to total area
+            if (islem[0] == "=") {
+                result1.value = total;
+                result2.value = "";
+            }
+            else {
+                result1.value = total + " " + islem[0] ;
+                result2.value = "";
+            }
+            
+    }
+
+    //action after press any number button 
+    if (e.target.classList.contains('num')){
+        sayi1 += e.target.value;
+        result2.value = sayi1;
+    }
+
+    //when user press ac button then everything is clear
+    if (e.target.classList.contains('btn1')){
+        total = "";
+        sayi1 = "";
+        sayi2 = "";
+        islem.shift();
+        result1.value = "";
+        result2.value = "";
+    }
+
+    // changing number multiply with minus operand
+    if (e.target.classList.contains('btn2')){
+        if (sayi1 != ""){
+            sayi1 = -(+sayi1)
+            result2.value = sayi1;
+        }
+        else {
+            total = -(total)
+            result2.value = total
+        }
+    }
+
 })
