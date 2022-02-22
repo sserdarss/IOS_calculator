3b42efbd02e1e7d3a9c735af5725fac95786f0c2
Serdar <serdar@gmail.com>
2022-02-22 15:47:37 +0100

    responsive changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //selectıng html elements
 let container = document.querySelector(".container");
-let result1 = document.getElementById("result1");
-let result2 = document.getElementById("result2");
+let result1 = document.getElementById("screen1");
+let result2 = document.getElementById("screen2");
 
 //variables created
 let total = "";
@@ -22,23 +22,6 @@ container.addEventListener("click", (e) => {
                 case "/" : total = +total/(+sayi1);break;
                 case "*" : total = +total*(+sayi1);break;
                 case "%" : total = (+total*+sayi1)/(100);break;
-                case "=" : {
-                    if (islem[0] == "=" && sayi1 == "") {
-                        result1.value = "";
-                        result2.value = total;
-                    }
-                    else {
-                    switch (islem[0]){
-                        case '+' : total = +sayi1+(+sayi2);break;
-                        case "-" : total = s2-s1 ;break;
-                        case "/" : total = +sayi1/(+sayi2);break;
-                        case "*" : total = +sayi1*(+sayi2);break;
-                        case "%" : total = (+total*+sayi1)/(100);break;
-                        default:break;
-                    }    
-                    
-                    }
-                }
                 default:break;
             }
             sayi1 = "";
@@ -47,7 +30,7 @@ container.addEventListener("click", (e) => {
         else if(total == "" ){
             total = sayi1;
             sayi1 = "";
-            result2.value = "";
+            result2.innerHTML = "";
             
         }
         //add  new operator in array and remove old operator
@@ -57,42 +40,77 @@ container.addEventListener("click", (e) => {
             }
             //thıs block for when only get one number and press "=" button then  move on number to total area
             if (islem[0] == "=") {
-                result1.value = total;
-                result2.value = "";
+
+                if ( total == Infinity){
+
+                result1.innerHTML= "ERROR";
+                result2.innerHTML = "";
+                total = 0
+
+                }
+                else{
+                    result1.innerHTML= total;
+                result2.innerHTML = "";
+
+                }
+                
             }
             else {
-                result1.value = total + " " + islem[0] ;
-                result2.value = "";
+                if ( total == Infinity){
+
+                    result1.innerHTML= "ERROR";
+                    result2.innerHTML = "";
+                    total = 0
+    
+                    }
+                    else{
+                result1.innerHTML = total + " " + islem[0] ;
+                result2.innerHTML = "";
             }
+            }
+            
             
     }
 
     //action after press any number button 
     if (e.target.classList.contains('num')){
         sayi1 += e.target.value;
-        result2.value = sayi1;
+        result2.innerHTML = sayi1;
     }
 
     //when user press ac button then everything is clear
     if (e.target.classList.contains('btn1')){
         total = "";
         sayi1 = "";
-        sayi2 = "";
         islem.shift();
-        result1.value = "";
-        result2.value = "";
+        result1.innerHTML = "";
+        result2.innerHTML = "";
     }
 
     // changing number multiply with minus operand
     if (e.target.classList.contains('btn2')){
         if (sayi1 != ""){
             sayi1 = -(+sayi1)
-            result2.value = sayi1;
+            result2.innerHTML = sayi1;
         }
         else {
             total = -(total)
-            result2.value = total
+            result2.innerHTML = total
         }
     }
 
 })
+
+
+//clock
+
+const updateTime = () => {
+  let clock = document.querySelector(".clock");
+  let currentTime = new Date();
+  let currentHour = currentTime.getHours();
+  let currentMinute = currentTime.getMinutes();
+  clock.innerHTML = currentHour + ":" + currentMinute;
+};
+
+setInterval(updateTime, 1000);
+updateTime();
